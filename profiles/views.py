ab38da6509c2ece8b34e8edffa09528b5b04d154
Arron <beale86@msn.com>
2022-11-15 18:33:14 +0000

    Add 404 page

--- profiles/views.py
+++ profiles/views.py
@@ -19,7 +19,7 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
         else:
-            messaged.error(request, 'Update failed. Please ensure the form is valid.')
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
 
